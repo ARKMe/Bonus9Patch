e25df89f38d901d2d5ad805193d3c0ba1d457170
andreabello <andrea91bis@hotmail.it>
2016-04-04 19:07:10 +0200

    9patch

---
+++ app/src/main/java/bello/andrea/bonus9patch/MainActivity.java
@@ -0,0 +1,13 @@
+package bello.andrea.bonus9patch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
